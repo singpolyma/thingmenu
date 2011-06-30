e9c6212512c0054e862f15cc3af6abd9f63fa10f
Christoph Lohmann <20h@r-36.net>
2011-06-30 20:05:59 +0200

    Bumping up the version number.

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
 # thingmenu metadata
 NAME = thingmenu
-VERSION = 0.2
+VERSION = 0.4
 
 # Customize below to fit your system
 
 # paths
-PREFIX ?= /usr
+PREFIX ?= /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
 X11INC = /usr/X11R6/include
